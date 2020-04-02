76f8ba0071d1583021a68ea3a94e1c04bf873f33
albert <amuluowin@163.com>
2020-04-02 17:04:28 +0800

    revert

--- src/Transaction.php
+++ src/Transaction.php
@@ -109,33 +109,28 @@ class Transaction
         if ($this->db === null) {
             throw new \InvalidArgumentException('Transaction::db must be set.');
         }
-        $this->_level++;
-        try {
-            $this->db->open();
+        $this->db->open();
 
-            if ($this->_level === 0) {
-                if ($isolationLevel !== null) {
-                    $this->db->getSchema()->setTransactionIsolationLevel($isolationLevel);
-                }
-                App::debug('Begin transaction' . ($isolationLevel ? ' with isolation level ' . $isolationLevel : ''), "db");
-                $this->db->pdo->beginTransaction();
-                $this->_level = 1;
-
-                return;
+        if ($this->_level === 0) {
+            if ($isolationLevel !== null) {
+                $this->db->getSchema()->setTransactionIsolationLevel($isolationLevel);
             }
+            App::debug('Begin transaction' . ($isolationLevel ? ' with isolation level ' . $isolationLevel : ''), "db");
+            $this->db->pdo->beginTransaction();
+            $this->_level = 1;
 
-            $schema = $this->db->getSchema();
-            if ($schema->supportsSavepoint()) {
-                App::debug('Set savepoint ' . $this->_level, "db");
-                $schema->createSavepoint('LEVEL' . $this->_level);
-            } else {
-                App::info('Transaction not started: nested transaction not supported', "db");
-                throw new NotSupportedException('Transaction not started: nested transaction not supported.');
-            }
-        } catch (\Throwable $exception) {
-            $this->_level--;
-            throw $exception;
+            return;
         }
+
+        $schema = $this->db->getSchema();
+        if ($schema->supportsSavepoint()) {
+            App::debug('Set savepoint ' . $this->_level, "db");
+            $schema->createSavepoint('LEVEL' . $this->_level);
+        } else {
+            App::info('Transaction not started: nested transaction not supported', "db");
+            throw new NotSupportedException('Transaction not started: nested transaction not supported.');
+        }
+        $this->_level++;
     }
 
     /**

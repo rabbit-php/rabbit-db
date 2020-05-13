f3ad4a53b7c8c7c06607f0db535b529147dba4b1
albert <amuluowin@163.com>
2020-05-13 14:24:26 +0800

    remove unuse code

--- src/Manager.php
+++ src/Manager.php
@@ -23,8 +23,6 @@ class Manager implements ReleaseInterface
     private $connections = [];
     /** @var array */
     private $deferList = [];
-    /** @var array */
-    private $yamlList = [];
     /** @var int */
     private $min = 48;
     /** @var int */
@@ -64,13 +62,6 @@ class Manager implements ReleaseInterface
     public function getConnection(string $name = 'db'): ?Connection
     {
         if (($connection = DbContext::get($name)) === null) {
-            /** @var PdoPool $pool */
-            if (!isset($this->connections[$name])) {
-                if (empty($this->yamlList)) {
-                    return null;
-                }
-                $this->createByYaml();
-            }
             $pool = $this->connections[$name];
             $connection = $pool->getConnection();
             DbContext::set($name, $connection);
@@ -100,41 +91,5 @@ class Manager implements ReleaseInterface
     public function release(): void
     {
         DbContext::release();
-    }
-
-    private function createByYaml(): void
-    {
-        foreach ($this->yamlList as $fileName) {
-            foreach (yaml_parse_file($fileName) as $name => $dbconfig) {
-                if (!isset($this->connections[$name])) {
-                    if (!isset($dbconfig['class']) || !isset($dbconfig['dsn']) ||
-                        !class_exists($dbconfig['class']) || !$dbconfig['class'] instanceof ConnectionInterface) {
-                        throw new Exception("The DB class and dsn must be set current class in $fileName");
-                    }
-                    [$min, $max, $wait] = ArrayHelper::getValueByArray(
-                        ArrayHelper::getValue($dbconfig, 'pool', []),
-                        ['min', 'max', 'wait'],
-                        null,
-                        $this->min,
-                        $this->max,
-                        $this->wait
-                    );
-                    ArrayHelper::removeKeys($dbconfig, 'class', 'dsn', 'pool');
-                    $this->connections[$name] = ObjectFactory::createObject([
-                        'class' => $dbconfig['class'],
-                        'dsn' => $dbconfig['dsn'],
-                        'pool' => ObjectFactory::createObject([
-                            'class' => \rabbit\db\pool\PdoPool::class,
-                            'poolConfig' => ObjectFactory::createObject([
-                                'class' => \rabbit\db\pool\PdoPoolConfig::class,
-                                'minActive' => intval($min / swoole_cpu_num()),
-                                'maxActive' => intval($max / swoole_cpu_num()),
-                                'maxWait' => $wait
-                            ], [], false)
-                        ], $dbconfig, false)
-                    ]);
-                }
-            }
-        }
     }
 }

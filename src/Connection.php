849192c17359b19087bb748521688ec5156d3fd9
Albert <amuluowin@163.com>
2022-04-15 14:22:55 +0800

    use memory for default cache

--- src/Connection.php
+++ src/Connection.php
@@ -55,7 +55,7 @@ class Connection extends BaseObject implements ConnectionInterface
     public bool $enableLogging = true;
     public int $maxLog = 1024;
     public int $share = 3;
-    public string $shareCache = 'share';
+    public string $shareCache = 'memory';
     public bool $canTransaction = true;
     protected string $transactionClass = Transaction::class;
     protected ?Schema $schema = null;
